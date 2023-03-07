ba6f404d591ac53e00ff5f29d9ec4552c1d7971f
Tajinboxx <TajLonghurstsoft@gmail.com>
2023-03-07 21:40:09 +1300

    Added Next.js scss

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
 /** @type {import("next").NextConfig} */
@@ -12,6 +18,9 @@ const config = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "scss")],
   },
 };
 export default config;
